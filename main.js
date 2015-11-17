881981e2d4113ecb311e2f586655b3e35bc61ff7
Anish Basu <anishbasu@acm.org>
2015-11-17 00:04:57 -0500

    Initial Commit

---
+++ main.js
@@ -0,0 +1,26 @@
+$(document).ready(function(){
+//Load Initial page
+$.get('intro.html', function(data){
+	$('#content').html(data)
+});
+//Load Page on Click
+$('#navpane a').click(
+	function(event){ 
+		event.preventDefault();
+		$('#nav-links li').css('background','')
+		$('#nav-links a').css('color','inherit')
+		if($(this).attr('id')!='heading'){
+			$(this).parent().css('background','#D0BDB4')
+			$(this).css('color','#003326')
+		}
+		$.ajax(
+			{
+				url: $(this).attr('href'),
+				success: function(response) {
+					$('#content').html(response);
+				}
+		})
+	return false;
+	}
+);
+});
